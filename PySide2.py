ae0f0b4e53a965e909d9e5fc8f9798730b84232f
Fujita Tatsuki <132869827+FugeTA@users.noreply.github.com>
2024-11-11 17:34:04 +0900

    Create PySide2.py

---
+++ PySide2.py
@@ -0,0 +1,8 @@
+# PySide2
+# やるべきこと
+self.setWindowTitle(title)  # タイトル決め
+self.setAttribute(QtCore.Qt.WA_DeleteOnClose)  # deleteLater()の自動実行
+self.setObjectName(objName(title))  # ウィジェットとしての名前付け
+# Mayaの場合widget作成は不要？
+
+# 機能
